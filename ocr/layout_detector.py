24e91cff645f8e90fcb3d0624f9edd009109a5e4
Polina <p.s.smirnova@mail.ru>
2026-01-18 18:45:07 +0300

    рефакторинг использования модели YOLO

--- ocr/layout_detector.py
+++ ocr/layout_detector.py
@@ -1,6 +1,15 @@
 from ultralytics import YOLO
 from PIL import Image
 from pathlib import Path
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s [OCR] %(levelname)s: %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 MODEL_PATH = Path(__file__).parent / "models" / "yolov8n.pt"
 
@@ -8,16 +17,7 @@ model = YOLO(str(MODEL_PATH))
 
 
 def detect_layout(image: Image.Image):
-    """
-    Возвращает список блоков:
-    [
-      {
-        "bbox": (x1, y1, x2, y2),
-        "label": "text",
-        "confidence": 0.92
-      }
-    ]
-    """
+    logger.info("YOLO START detect_layout")
     results = model(image)
     blocks = []
     for r in results:
@@ -25,7 +25,7 @@ def detect_layout(image: Image.Image):
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             cls_id = int(box.cls[0])
             label = model.names[cls_id]
-
+            logger.info(f"Detected class: {label}")
             blocks.append({
                 "bbox": (x1, y1, x2, y2),
                 "label": label,
--- ocr/pipeline.py
+++ ocr/pipeline.py
@@ -20,30 +20,32 @@ def analyze_document(image: Image.Image):
     result_blocks = []
     full_text_parts = []
 
-    # === YOLO ничего не нашёл ===
     if len(blocks) == 0:
-        logger.warning("YOLO returned no blocks, fallback to full image OCR")
+        # === YOLO ничего не нашёл ===
+        logger.warning("---------YOLO ничего не нашёл---------")
         text = recognize_text(image)
-
-        return {
-            "blocks": [],
-            "full_text": text
-        }
-
-    # === YOLO нашёл блоки ===
-    for block in blocks:
-        x1, y1, x2, y2 = block["bbox"]
-        cropped = image.crop((x1, y1, x2, y2))
-
-        text = recognize_text(cropped)
-        full_text_parts.append(text)
-
+        w, h = image.size
         result_blocks.append({
-            "bbox": block["bbox"],
-            "label": block.get("label", "text"),
-            "confidence": block.get("confidence"),
-            "text": text
+            "bbox": (0, 0, w, h),
+            "label": "full_page",
+            "confidence": 1.0,
+            "text": text,
         })
+    else:
+        # === YOLO нашёл блоки ===
+        for block in blocks:
+            x1, y1, x2, y2 = block["bbox"]
+            cropped = image.crop((x1, y1, x2, y2))
+
+            text = recognize_text(cropped)
+            full_text_parts.append(text)
+
+            result_blocks.append({
+                "bbox": block["bbox"],
+                "label": block.get("label", "text"),
+                "confidence": block.get("confidence"),
+                "text": text
+            })
 
     return {
         "blocks": result_blocks,
